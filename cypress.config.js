5d2a0433b20b8f6df3f354a8bcf08da8d5a229bc
Zaicescu Paul <p.zaicescu@gmail.com>
2023-03-13 16:57:51 +0200

    adding workflows

--- cypress.config.js
+++ cypress.config.js
@@ -1,11 +1,17 @@
 const {defineConfig} = require("cypress");
 
 module.exports = defineConfig({
-    e2e: {
-        setupNodeEvents(on, config) {
-            // implement node event listeners here
-        },
-      specPattern: "cypress/e2e/**/*.{js,jsx,ts,tsx,feature}",
-      chromeWebSecurity: false,
+  reporter: 'cypress-mochawesome-reporter',
+  reporterOptions: {
+    charts: true,
+    reportPageTitle: 'Demo Report',
+    embeddedScreenshots: true,
+  },
+  e2e: {
+    setupNodeEvents(on, config) {
+      require('cypress-mochawesome-reporter/plugin')(on);
     },
+    specPattern: "cypress/e2e/**/*.{js,jsx,ts,tsx,feature}",
+    chromeWebSecurity: false,
+  },
 });
